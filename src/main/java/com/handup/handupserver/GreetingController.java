bfc6f116ceef20fb507f4662fc5d1868f6f346e6
Andrew <merrillman32@gmail.com>
2019-06-17 18:51:35 -1000

    changed mapping for controller

--- src/main/java/com/handup/handupserver/GreetingController.java
+++ src/main/java/com/handup/handupserver/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class GreetingController {
 
-    @RequestMapping("/")
+    @RequestMapping("/greeting")
     public String index() {
         return "Greetings from the Greeting controller!";
     }
